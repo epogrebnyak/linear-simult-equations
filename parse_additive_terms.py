fd0ab81d64646f06da4205e80a68b19b3bcc4be3
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2015-07-06 20:46:08 +0300

    Update parse_additive_terms.py

--- parse_additive_terms.py
+++ parse_additive_terms.py
@@ -1,4 +1,8 @@
-
+"""
+   Test case to convert text strings to DataFrame-friendly dictionary. 
+   
+   Suggested library: Sympy
+"""
 multipliers = {		
 	'liq_share':  0.2	
 ,	'credit_ir ': 0.1	
@@ -12,25 +16,26 @@ equations =  [
 ,	'fgap = ta - capital - profit - deposit'	
 ]	
 
+free_term_var = 'b'
+
 # from equations and values we must get follwoing:
-free_term = 'b'
 
 structured_equations = [   
-    {'ta': -1,     free_term:0, 'credit': 1, 'liq':1}
-,	{'liq': -1,    free_term:0, 'credit': 0.2}	
-,	{'profit': -1, free_term:0, 'credit': 0.1, 'deposit':-0.05}	
-,	{'fgap':-1,    free_term:0, 'ta':1, 'capital':-1, 'profit':-1, 'deposit': -1}
+    {'ta': -1,     free_term_var:0, 'credit': 1, 'liq':1}
+,	{'liq': -1,    free_term_var:0, 'credit': 0.2}	
+,	{'profit': -1, free_term_var:0, 'credit': 0.1, 'deposit':-0.05}	
+,	{'fgap':-1,    free_term_var:0, 'ta':1, 'capital':-1, 'profit':-1, 'deposit': -1}
 ]
 
-def split_equation_string_to_dictionary(equation, multipliers, free_term = 'b'):
+def split_equation_string_to_dictionary(equation, multipliers, free_term = free_term_var):
     """
     Must return a dictionary as in 'structured_equations'. Rules:
     
-    Easy ones
+    Easy ones:
     1. left-hand side variable ('ta', 'liq') is assigned -1
     2. free_term always 0
     
-    The gist of it 
+    The gist of it:
     3. all right-hand additive terms must be split into sign, variable, and multiplier and evaluated 
         Example: for "- deposit * deposit_ir"
                     sign is -1
