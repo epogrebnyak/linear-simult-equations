2b5ea3dfb3d8e4dc24fb1d789ad84fa5f8d37aa7
=epogrebnyak <e.pogrebnyak@gmail.com>
2015-07-14 01:14:24 +0300

    Refactoring finished
    Please proceed to # CHECK 1 and #TODO 3

--- input_example.py
+++ input_example.py
@@ -19,39 +19,64 @@ csvfile.ix[:, 1] = csvfile.ix[:, 1].str.strip()
 # produce 'mutipliers', 'equations', 'values 'in data structures similar to ones below
 # use period 0 data
 
-multiplier_directives = csvfile[csvfile.ix[:, 0] == 'multiplier']
-multipliers = dict(multiplier_directives.ix[:,1:3].values)
-print ('Multipliers')
+def get_csv_block_as_dict(csvfile, pivot_label, val_col = 2, key_col = 1):
+    directives = csvfile[csvfile.ix[:, 0] == pivot_label]
+    value_dict = dict(directives.ix[:,(key_col,val_col)].values)
+    return  value_dict
+
+def get_multipliers_as_dict(csvfile):
+    return get_csv_block_as_dict(csvfile, 'multiplier')    
+    
+def get_values_as_dict(csvfile, period):
+    return get_csv_block_as_dict(csvfile, 'value', val_col = 2 + period)
+
+def get_equations_as_list(csvfile):
+    return list(get_csv_block_as_dict(csvfile, 'equation').keys())
+    
+multipliers = get_multipliers_as_dict(csvfile)
+values      = get_values_as_dict(csvfile, period)
+equations   = get_equations_as_list(csvfile)
+
+print ('\nMultipliers:')
 pprint(multipliers)
-print ('---')
-#multipliers = {
-#	'liq_share':  .20
-#,	'credit_ir': .10
-#,	'deposit_ir': .05
-#}
-
-equation_directives = csvfile[csvfile.ix[:, 0] == 'equation']
-equations = equation_directives.ix[:,1].values.tolist()
-print ('Equations')
-pprint(equations)
-print ('--')
-#equations =  [
-#	'ta = credit + liq'
-#,	'liq = credit * liq_share'
-#,	'profit = credit * credit_ir - deposit * deposit_ir'
-#,	'fgap = ta - capital - profit - deposit'
-#]
-
-value_directives = csvfile[csvfile.ix[:, 0] == 'value']
-values = dict(value_directives.ix[:,(1,2)].values)
-print ('Values')
+print ('\nValues:')
 pprint(values)
-print ('--')
-#values = {
-#	'capital': 100
-#,	'credit': 500
-#,	'deposit': 300
-#}
+print ('\nEquations:')
+pprint(equations)
+
+# # multiplier_directives = csvfile[csvfile.ix[:, 0] == 'multiplier']
+# # multipliers = dict(multiplier_directives.ix[:,1:3].values)
+# # print ('Multipliers')
+# # pprint(multipliers)
+# # print ('---')
+# multipliers = {
+	# 'liq_share':  .20
+# ,	'credit_ir': .10
+# ,	'deposit_ir': .05
+# }
+
+# # equation_directives = csvfile[csvfile.ix[:, 0] == 'equation']
+# # equations = equation_directives.ix[:,1].values.tolist()
+# # print ('Equations')
+# # pprint(equations)
+# # print ('--')
+# equations =  [
+	# 'ta = credit + liq'
+# ,	'liq = credit * liq_share'
+# ,	'profit = credit * credit_ir - deposit * deposit_ir'
+# ,	'fgap = ta - capital - profit - deposit'
+# ]
+
+# # value_directives = csvfile[csvfile.ix[:, 0] == 'value']
+# # values = dict(value_directives.ix[:,(1,2)].values)
+# # print ('Values')
+# # pprint(values)
+# # print ('--')
+# values = {
+	# 'capital': 100
+# ,	'credit': 500
+# ,	'deposit': 300
+# }
 
 # WARNING: solve_lin_system is undefined
 #x = solve_lin_system(multipliers, equations, values)
